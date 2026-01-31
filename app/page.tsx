f54055f1b54e8525dabaf0462175789c73303c0d
sunnyJha08 <sunnyjha98971@gmail.com>
2026-01-31 22:06:23 +0530

    feat: add theme provider and animated theme toggler components
    - Implemented ThemeProvider component for managing themes using next-themes.
    - Created AnimatedThemeToggler component for toggling between light and dark themes with animations.
    - Added DotPattern component for creating animated dot patterns as backgrounds.
    - Introduced MagicCard component for interactive cards with gradient effects based on mouse position.
    - Developed RetroGrid component for a customizable retro grid background.
    - Added Ripple component for ripple effect animations on elements.
    - Updated package.json to include next-themes dependency.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
+import { DotPattern } from "@/components/ui/dot-pattern";
 import { Globe } from "@/components/ui/globe";
+import { MagicCard } from "@/components/ui/magic-card";
+import { RetroGrid } from "@/components/ui/retro-grid";
+import { Ripple } from "@/components/ui/ripple";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Globe />
-    </div>
+    <>
+      <DotPattern />
+    </>
   );
 }
